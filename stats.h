b5c3633a3fb4fee4afc610a4a620f0683a96c982
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-01-28 19:06:35 +0530

    Create stats.h
    update header with stats structure

--- stats.h
+++ stats.h
@@ -1,6 +1,21 @@
+#include <stdlib.h>
 
+/*=============================================================================
+=======                       CONSTANTS  &  TYPES                       =======
+=============================================================================*/
+
+/*structure for stats*/
+struct Stats 
+{
+ float average; 
+ float min;
+ float max;
+};
+
+/*=============================================================================
+=======                      FUNCTION DECLARATIONS                     =======
+=============================================================================*/
 struct Stats compute_statistics(const float* numberset, int setlength);
-
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
