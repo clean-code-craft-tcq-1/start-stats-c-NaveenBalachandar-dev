efb42fc50404d13ab77b6f5921bba1bab7462de1
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-01-28 20:58:09 +0530

    Update stats.c
    test case1 vad

--- stats.c
+++ stats.c
@@ -1,6 +1,8 @@
 #include "stats.h"
 
 static float compute_avgerage_f(const float* numberset, int setlength);
+static float compute_MinNum_f(const float* numberset, int setlength);
+static float compute_MaxNum_f(const float* numberset, int setlength);
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     
@@ -8,9 +10,10 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     
     /*computing average*/
     s.average = compute_avgerage_f(numberset ,setlength);
-   
-    s.min = numberset[1]; /*to be done*/
-    s.max = numberset[0]; /*to be done*/
+   /*computing average*/
+    s.min = compute_MinNum_f(numberset ,setlength); 
+    /*computing average*/
+    s.max = compute_MaxNum_f(numberset ,setlength);
     return s;
 }
 
@@ -31,6 +34,43 @@ static float compute_avgerage_f(const float* numberset, int setlength)
    return avg; 
 }
 
+static float compute_MinNum_f(const float* numberset, int setlength)
+{
+  static float minNum = numberset[0];  
+  int count;
+  
+   /*calculating Min number in array*/
+  for (count=1 ;count<setlength;count++)  
+  {
+      if (minNum > numberset[count])
+      {
+         minNum = numberset[count];
+      }
+  }
+
+    /*calculating avg*/  
+    
+   return minNum; 
+}
+
+static float compute_MaxNum_f(const float* numberset, int setlength)
+{
+  static float Maxnum = numberset[0];  
+  int count;
+  
+   /*calculating Min number in array*/
+  for (count=1 ;count<setlength;count++)  
+  {
+      if (Maxnum < numberset[count])
+      {
+         Maxnum = numberset[count];
+      }
+  }
+
+    /*calculating avg*/  
+    
+   return Maxnum; 
+}
 //Test code to check test case 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
