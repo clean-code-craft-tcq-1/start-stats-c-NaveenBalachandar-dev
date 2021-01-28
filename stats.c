4bf92a068dfaab68b413b5131488f67fe44dffb9
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-01-28 18:17:41 +0530

    Update stats.c
    test code

--- stats.c
+++ stats.c
@@ -1,10 +1,18 @@
 #include "stats.h"
 
+typedef struct Stats 
+{
+ int average;
+ int min;
+ int max;
+}
+
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    //s.average = 0;
-   // s.min = 0;
-    //s.max = 0;
+    s.average = 0;
+    s.min = 0;
+    s.max = 0;
 }
 
 //Test code to check test case 
