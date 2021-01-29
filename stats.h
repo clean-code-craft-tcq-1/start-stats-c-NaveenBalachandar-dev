21f4ea6e141bac144417b6b2d0f167b2c44fd331
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-01-29 17:43:16 +0530

    Update stats.h
    removing counter from header

--- stats.h
+++ stats.h
@@ -29,9 +29,3 @@ struct Stats
 struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-
-/*=============================================================================
-=======                      COMMON VARIABLES                           =======
-=============================================================================*/
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
